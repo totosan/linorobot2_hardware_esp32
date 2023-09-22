c93f35c74ed938377f370192bf7c5af2f9596121
robofoundry <foundryrobo@gmail.com>
2023-09-22 12:01:40 -0400

    updated to fix lot of motor values, pin numbers after testing with DFRobot TTmotors

--- config/custom/myrobot_config.h
+++ config/custom/myrobot_config.h
@@ -15,7 +15,7 @@
 #ifndef MYROBOT_CONFIG_H
 #define MYROBOT_CONFIG_H
 
-#define LED_PIN 13 
+#define LED_PIN 2
 #define ESP32
 #define SOC_PCNT_SUPPORTED
 #define BAUDRATE 115200
@@ -27,23 +27,32 @@
 #define SDA_PIN 21 // specify I2C pins for esp32
 #define SCL_PIN 22
 
+// #define USE_LIDAR_UDP
+// #define LIDAR_RXD 17 // you may use any available input pin
+// // #define LIDAR_PWM 15  // do not use, the PWM control loop is not implememted yet
+// #define LIDAR_SERIAL 1 // uart number, 1 or 2
+// #define LIDAR_BAUDRATE 230400 // the Lidar serial buadrate
+// #define LIDAR_SERVER { 192, 168, 1, 145 }  // eg your desktop IP addres
+// #define LIDAR_PORT 8889 // the UDP port on server
+
+
 #define K_P 0.6                             
 #define K_I 0.8                             
 #define K_D 0.5                             
 
-#define MOTOR_MAX_RPM 300       
-#define MAX_RPM_RATIO 0.50
-#define MOTOR_OPERATING_VOLTAGE 12
-#define MOTOR_POWER_MAX_VOLTAGE 12
-#define MOTOR_POWER_MEASURED_VOLTAGE 12                  
-#define COUNTS_PER_REV1 438
-#define COUNTS_PER_REV2 446
+#define MOTOR_MAX_RPM 160       
+#define MAX_RPM_RATIO 0.9
+#define MOTOR_OPERATING_VOLTAGE 6
+#define MOTOR_POWER_MAX_VOLTAGE 6
+#define MOTOR_POWER_MEASURED_VOLTAGE 5.4            
+#define COUNTS_PER_REV1 960
+#define COUNTS_PER_REV2 960
 #define COUNTS_PER_REV3 240
 #define COUNTS_PER_REV4 240
 #define WHEEL_DIAMETER 0.152               
 #define LR_WHEELS_DISTANCE 0.271            
 #define PWM_BITS 8                         
-#define PWM_FREQUENCY 5000
+#define PWM_FREQUENCY 30000
 
 // Fixed pin numbers for ESP32-WROOM-32D 38 PIN VERSION
 /// ENCODER PINS
@@ -91,7 +100,7 @@
 #define AGENT_PORT 8888
 // Enable WiFi with null terminated list of multiple APs SSID and password
 #define WIFI_AP_LIST {{"WIFI_SSID", "WIFI_PASSWORD"}, {NULL}}
-// #define WIFI_MONITOR 2 // min. period to send wifi signal strength to syslog
+#define WIFI_MONITOR 2 // min. period to send wifi signal strength to syslog
 // #define USE_ARDUINO_OTA
 #define USE_SYSLOG
 #define SYSLOG_SERVER { 192, 168, 1, 100 }  // eg IP of the desktop computer
